ec904e15b26d282df9f7970ffc8ca84713d4114b
klesseranaren <klesser@anaren.com>
2016-06-08 14:08:02 -0400

    fix scope error in element code

--- element_code/Acceleration.c
+++ element_code/Acceleration.c
@@ -1,7 +1,7 @@
 	BMC150_accel_t accel = {0};
 	BMC150_read_accel(&accel);
 	
-	char valueBuffer[32];
+	static char valueBuffer[32];
 	
 	sprintf(valueBuffer, "[%d,%d,%d]", accel.x, accel.y, accel.z);
 	
--- element_code/MagneticField.c
+++ element_code/MagneticField.c
@@ -1,7 +1,7 @@
 	BMC150_mag_t mag = {0};
 	BMC150_read_mag(&mag);
 	
-	char valueBuffer[32];
+	static char valueBuffer[32];
 	
 	sprintf(valueBuffer, "[%d,%d,%d]", mag.x, mag.y, mag.z);
 	
